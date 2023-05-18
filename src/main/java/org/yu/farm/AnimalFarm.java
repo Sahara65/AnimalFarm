3874f2dafe13cefc5bbbfc1d81af41936a08cb72
Paul Kimball <pekimball@interfaceassociates.net>
2023-05-18 16:48:01 -0400

    Added Scary Animals

--- src/main/java/org/yu/farm/AnimalFarm.java
+++ src/main/java/org/yu/farm/AnimalFarm.java
@@ -1,31 +1,27 @@
 package org.yu.farm;
 
 import org.yu.farm.scary.Jaguar;
+import org.yu.farm.scary.ScaryAnimal;
+import org.yu.farm.scary.ScaryCat;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class AnimalFarm {
 
-
     // Member variables
     private List<Animal> animals = new ArrayList<>();
 
-
-
-
-
     public List<Animal> getAnimals() {
-
         return animals;
     }
 
 
-
-
-
     // Constructor
     public AnimalFarm() {
+
+        // ALL Animals are welcome at Animal Farm
+        // Even Scary ones.
 
         animals.add(    new Horse("Benny")     );
         animals.add(    new Armadillo("Susan")     );
@@ -35,7 +31,8 @@ public class AnimalFarm {
         animals.add(    new Cow( "Sweet Melissa")     );
         animals.add(    new Pig("Theophanes")     );
         animals.add(    new Cat("Bai-Bai")      );
-        animals.add(    new Jaguar("Bob the Bad")  );
+        animals.add(    new Jaguar("Bob the not so Bad")  );
+        animals.add(    new ScaryCat("Zoe (Not really bad; just a little scary)"));
     }
 
 }
